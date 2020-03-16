f1ed33b875d0803e74a301402eac7ddc9925912c
Manga08 <bijuu5445@gmail.com>
2020-03-15 19:48:21 -0500

    [Update]: The printf and print_functions files

---
+++ print_functions.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * p_int - integer
+ *@data: type data.
+ *@buffer: the buffer
+ *@size: the size of the array
+ */
+void p_int (va_list data, char *buffer, unsigned int *size)
+{
+	int i = va_arg(data, int), n, sizes = 0, tmps = 0;
+
+	n = 1;
+	while (n / 10 > 0)
+	{
+		n /= 10;
+		sizes++;
+	}
+	sizes++;
+	tmps = sizes;
+	while (sizes > 0)
+	{
+		buffer[*size + sizes - 1] = (i % 10) + 48;
+		i /= 10;
+		sizes--;
+	}
+	*size += tmps;
+}
