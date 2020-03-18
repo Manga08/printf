40783c2a7d77ada9163f46a4052e5e6912f1cbbc
Manga08 <bijuu5445@gmail.com>
2020-03-17 20:45:45 -0500

    [Update]: fixing the code issues

--- _printf.c
+++ _printf.c
@@ -8,13 +8,12 @@ int _printf(const char *format, ...)
 {
 	char *buffer = malloc(2048);
 	va_list data;
-	unsigned int cf = 0, ca = 0, m = 0, *size = 0;
+	unsigned int cf = 0, ca = 0, m = 0, *size = &m;
 	cases porcentage[] = {
-		{'c', p_char},{'i', p_int},{'s', p_str},{'d', p_int},
-		{'%', p_por},{'u', p_int},{'o', p_int},{'x', p_int},
+		{'c', p_char}, {'i', p_int}, {'s', p_str}, {'d', p_int},
+		{'%', p_por}, {'u', p_unsig}, {'x', p_unsig}, {'X', p_unsig},
 		{0, NULL}
 	};
-	size = &m;
 	if (!buffer || !format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(data, format);
@@ -40,6 +39,7 @@ int _printf(const char *format, ...)
 		}
 		cf++;
 	}
+	buffer[*size] = '\0';
 	write(1, buffer, *size);
 	va_end(data);
 	free(buffer);
--- print_functions.c
+++ print_functions.c
@@ -12,7 +12,7 @@ void p_int(va_list data, char *buffer, unsigned int *size)
 	if (m < 0)
 	{
 		m *= -1;
-		buffer[*size]= '-';
+		buffer[*size] = '-';
 		*size += 1;
 	}
 
@@ -43,21 +43,14 @@ void p_char(va_list data, char *buffer, unsigned int *size)
 	*size += 1;
 }
 /**
- * p_dec - integer
+ * p_unsig - integer
  *@data: type data.
  *@buffer: the buffer
  *@size: the size of the array
  */
-void p_dec(va_list data, char *buffer, unsigned int *size)
+void p_unsig(va_list data, char *buffer, unsigned int *size)
 {
-	long int m = va_arg(data, int), i = 0, n = m, tmp;
-
-	if (m < 0)
-	{
-		m *= -1;
-		buffer[*size]= '-';
-		*size += 1;
-	}
+	unsigned int m = va_arg(data, int), i = 0, n = m, tmp;
 
 	while (n / 10)
 	{
@@ -82,8 +75,8 @@ void p_dec(va_list data, char *buffer, unsigned int *size)
  */
 void p_str(va_list data, char *buffer, unsigned int *size)
 {
-	int cnt = 0;
 	char *contain = va_arg(data, char*);
+	int cnt = 0;
 
 	if (contain == NULL)
 		contain = "(null)";
